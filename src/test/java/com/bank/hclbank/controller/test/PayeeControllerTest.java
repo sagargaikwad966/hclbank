86255c9d8960f0ed70e7ad7f491674844f6150ed
ckale12 <kalechaitali1993@gmail.com>
2019-07-26 17:55:35 +0530

    junit controller test

--- src/test/java/com/bank/hclbank/controller/test/PayeeControllerTest.java
+++ src/test/java/com/bank/hclbank/controller/test/PayeeControllerTest.java
@@ -2,10 +2,12 @@ package com.bank.hclbank.controller.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -17,6 +19,7 @@ import org.springframework.http.ResponseEntity;
 import com.bank.hclbank.controller.PayeeController;
 import com.bank.hclbank.entity.Payee;
 import com.bank.hclbank.exception.ApplicationException;
+import com.bank.hclbank.model.PayeeRequestModel;
 import com.bank.hclbank.model.ResponseData;
 import com.bank.hclbank.service.PayeeService;
 
@@ -28,6 +31,26 @@ public class PayeeControllerTest {
 
 	@Mock
 	PayeeService payeeServiceMock;
+        
+        
+	Payee payee;
+	Payee payee1;
+	
+	PayeeRequestModel payeeRequestModel;
+	
+	@Before
+	public void setup() {
+	payee=new Payee();
+	payee.setPayeeAccountNumber(12345L);
+	payee.setPayeeId(1L);
+	payee.setPayerAccountNumber(123890L);
+	payee.setStatus("active");
+	
+	payeeRequestModel=new PayeeRequestModel();
+	payeeRequestModel.setPayeeAccountNumber(12345L);
+	payeeRequestModel.setPayerAccountNumber(123789L);
+	
+	}
 
 	@Test
 	public void viewBeneficiaries() throws ApplicationException {
@@ -44,6 +67,30 @@ public class PayeeControllerTest {
 
 		assertNotNull(response);
 		assertEquals(200, response.getStatusCodeValue());
+	}
+        
+
+	@Test
+	public void testRemovePayee() throws ApplicationException {
+		Long payeeId=1L;
+		
+		when(payeeServiceMock.removePayee(payeeId)).thenReturn(payee);
+		ResponseEntity<ResponseData> response =payeeControllerMock.removePayee(payeeId);
+		assertNotNull(response);
+		
+	}
+	@Test(expected = ApplicationException.class)
+	public void testRemovePayeeException()throws ApplicationException{
+		ResponseEntity<ResponseData> response =payeeControllerMock.removePayee(null);
+	}
+	
+	@Test
+	public void testAddPayee() throws ApplicationException {
+		
+	      when(payeeServiceMock.addPayee(payeeRequestModel)).thenReturn(payee);
+	      ResponseEntity<ResponseData> response =payeeControllerMock.addPayee(payeeRequestModel);
+			assertNotNull(response);
+			
 	}
 
 }
